62fad69260104841c202450d96f07ed1cc8d14dc
Seif Tarek <seift6522@gmail.com>
2026-05-30 20:03:31 +0300

    fix model loading

--- utils/model_loader.py
+++ utils/model_loader.py
@@ -1,6 +1,8 @@
 import streamlit as st
 from ultralytics import YOLO
+import os
+
 @st.cache_resource
 def load_model():
-    return YOLO(r"C:\Users\Lenovo\PycharmProjects\PythonProject1\model\best.pt")
-model = load_model()
+    model_path = os.path.join("model", "best.pt")
+    return YOLO(model_path)
